5bba5c2632e81b25d6b9fb647de30f7621c82cb2
genebit <genebitara@gmail.com>
2021-08-29 20:12:30 +0800

    Finished convertion from png to jpg (HARD CODED) TODO: Create am input for what format to convert

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from PIL import Image
 import os
 
-def convert(filename, new_format):
-	img = Image.open(filename).convert('RGB')
-	img.save(filename, new_format)
 
-def main:
-	directory = input('Enter a Directory\n> ')
+def convert(filename, extension, new_format):
+    img = Image.open(f'./test/{filename}{extension}').convert('RGB')
+    img.save(f'./dest/{filename}.{new_format}', new_format)
 
-	print('Contents: ')
 
-	num = 0
-	for file in os.listdir(directory):
-		num += 1
+def main():
+    directory = input('Enter a Directory\n> ')
+
+    print('Contents: ')
+
+    num = 0
+    for file in os.listdir(directory):
+        num += 1
         filename, extension = os.path.splitext(file)
-        
-        if extension == '.PNG':
-        	print(' {0}.\t{1}{2}'.format(num, filename, extension))
 
-        	convert(f'{directory}\\{filename}.{extension}', 'JPEG')
+        if extension == '.PNG':
+            print(' {0}.\t{1}{2}'.format(num, filename, extension))
+
+            convert(filename, extension, 'JPEG')
 
 if __name__ == '__main__':
-	main()
-		
-	print('Convertion Done')
+    main()
+
+    print('Convertion Done')
